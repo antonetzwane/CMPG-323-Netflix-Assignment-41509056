061433652fc348069cfae703422705c7d83ba705
A Zwane Zwane <41509056@mynwu.ac.za>
2025-01-24 09:56:28 +0200

    Updated Interface Repository

---
+++ Repository/NetInterfaceClass.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using OfficeOpenXml;
+using NetflixAPISolution.Models;
+
+namespace NetflixAPISolution.Repository
+{
+    public interface NetflixInterface
+    {
+        List<NetflixData> GetAll(); //Retrieves all items.
+        NetflixData GetById(int id); // GET by ID.
+        void Add(NetflixData netflixData); // Using POST to create a new entry in the database.
+        void Update(int id, NetflixData updatedNetflixData); // PUT/PATCH methods to update an existing entry in the database.
+        void Delete(int id); // DELETE to r emove an existing entry.
+        void FetchFromExcel(string filePath); // FETCH dataset from Excel.
+        void Deduplicate(); // Remove duplicates from the dataset.
+    }
+}
