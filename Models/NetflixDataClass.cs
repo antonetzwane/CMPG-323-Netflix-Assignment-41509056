fa858731c97e379f9a5e8fdbaa2909837e67a8c2
A Zwane Zwane <41509056@mynwu.ac.za>
2025-01-24 09:45:44 +0200

    Updated Netflix Data Class

---
+++ Models/NetflixDataClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NetflixAPISolution.Models
+{
+    public class NetflixData
+    {
+        public int Id { get; set; }
+        public string? title { get; set; } = null;
+        public string? rating { get; set; } = null;
+        public string? ratingLevel { get; set; }
+        public int ratingDescription { get; set; }
+        public int releaseyear { get; set; }
+        public int userratingscore { get; set; }
+        public int userratingsize { get; set; }
+
+        //NetflixExcel ex = new NetflixExcel(@"C:\Users\zwane\Downloads\netflix.xlsx", 1);//netflix.xlsx"
+    }
+}
+
+
