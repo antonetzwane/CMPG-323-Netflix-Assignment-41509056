e6059a7178dc76b7cfe6b3b8adfba3359ed0d974
A Zwane Zwane <41509056@mynwu.ac.za>
2025-01-24 10:01:33 +0200

    Updated Programs.cs

--- Programs.cs
+++ Programs.cs
@@ -1,8 +1,35 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using NetflixAPISolution.Models;
+using NetflixAPISolution.Repository;
 
-public class Class1
+var builder = WebApplication.CreateBuilder(args);
+
+
+//Code to load the repository class into controller class.
+builder.Services.AddSingleton<NetflixRepository, NetflixRepository>();
+
+var filePath = @"C:\Users\zwane\Downloads\netflix.xlsx";
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-	public Class1()
-	{
-	}
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+//Push this to github...
